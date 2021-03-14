abc75beab59e19113ebd75b750876c41946ac023
yangfan19 <yangfan19@100tal.com>
2021-03-14 19:09:36 +0800

    增加解析字体、图片配置

---
+++ webpack-07/src/search.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import './search.css';
+import './search01.less';
+import logo from './images/im1.jpg';
+
+const Search = () => {
+  return (
+    <div className='box'>
+      我是search组件
+      <img src={logo} />
+    </div>
+  );
+};
+
+ReactDOM.render(<Search />, document.getElementById('root'));
